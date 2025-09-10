ca6952bac5bcd0a1603fd56f0894b68c974c134b
semen_kosyachenko <skosyachenko@optisoft.net>
2025-09-10 10:09:59 +0300

    no message

--- src/example_run.py
+++ src/example_run.py
@@ -1,19 +1,18 @@
 from src.graph_generator import GraphGenerator
 from src.tools import build_sunkey_links
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     gg = GraphGenerator([2, 2])
     gg.build()
 
     graph = gg.fill_with_values(100)
-    gg.build_train_dataset(ln=2, errors_ratio=.2, to_int=True)
+    gg.build_train_dataset(ln=2, errors_ratio=0.2, to_int=True)
 
     df = gg.build_pivot_table(10)
 
-    #print(df.head())
+    # print(df.head())
 
     sunkey_diagram_links = build_sunkey_links(graph)
-
 
     # строим свертку
     # считаем максимальный ранг графа
@@ -21,4 +20,4 @@ if __name__ == '__main__':
     for node in graph.nodes.values():
         rank = max(rank, len(node.edges_in), len(node.edges_out))
 
-    #for node in graph.nodes.values():
+    # for node in graph.nodes.values():
